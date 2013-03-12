af7c0f0463f8ad1c6db85512c6d2126c5c164b8e
Matt Erickson <matt@spideroak.com>
2013-03-12 00:07:02 -0500

    Updates to IRC and email again.

--- init.d/30_irc.el
+++ init.d/30_irc.el
@@ -3,8 +3,8 @@
 
 (defun mce-growl-at-me (match-type nick message)
   (unless (posix-string-match "^\\** *Users on #" message)
-    (todochiku-message (concat "ERC: " (buffer-name (current-buffer)))
-                       (concat nick ": " message)
+    (todochiku-message (concat "ERC: " (buffer-name (current-buffer)) ": " nick)
+                       message
                        (todochiku-icon 'irc))))
 
 (add-hook 'erc-text-matched-hook 'mce-growl-at-me)
