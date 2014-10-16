0ae4bb64fb1cf4d881387d795549767cd16dd049
Matt Erickson <matt@spideroak.com>
2014-10-16 18:41:37 -0400

    Latest updates.

--- init.d/30_twitter.el
+++ init.d/30_twitter.el
@@ -1,8 +1,8 @@
-(require 'twittering-mode)
+;; (require 'twittering-mode)
 
-;; Use the master password for twittering mode.
-(setq twittering-use-master-password t)
+;; ;; Use the master password for twittering mode.
+;; (setq twittering-use-master-password t)
 
-;; Auto-update the timeline, and show cute icons.
-(setq twittering-icon-mode t)
-(setq twittering-timer-interval 300)
+;; ;; Auto-update the timeline, and show cute icons.
+;; (setq twittering-icon-mode t)
+;; (setq twittering-timer-interval 300)
