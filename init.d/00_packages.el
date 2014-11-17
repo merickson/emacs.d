96d4ee38561817785c4e380f19dcdfbce68f833f
Matt Erickson <matt@spideroak.com>
2014-11-16 20:59:23 -0600

    Added HTTPS for Marmalade

--- init.d/00_packages.el
+++ init.d/00_packages.el
@@ -1,7 +1,7 @@
 ; Configure packages
 (setq package-archives
       '(("gnu" . "http://elpa.gnu.org/packages/")
-	("marmalade" . "http://marmalade-repo.org/packages/")
+	("marmalade" . "https://marmalade-repo.org/packages/")
         ("melpa" . "http://melpa.milkbox.net/packages/")
         ("org" . "http://orgmode.org/elpa/")))
 (package-initialize)
