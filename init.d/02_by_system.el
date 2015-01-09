cebe6a9127e76496109ddc3fbdfe2421b25add3d
Matt Erickson <matt@spideroak.com>
2015-01-09 09:22:57 -0600

    Fixed missing progn for the if statement in 02_by_system.

--- init.d/02_by_system.el
+++ init.d/02_by_system.el
@@ -6,7 +6,7 @@
    (setq ns-alternate-modifier 'none)))
 
 (if (string= system-type "windows-nt")
-    ((setq mce-so-hive "~/Documents/SpiderOak Hive/")
-     (setq tramp-default-method "plink"))
+    (progn (setq mce-so-hive "~/Documents/SpiderOak Hive/")
+	   (setq tramp-default-method "plink"))
   (setq mce-so-hive "~/SpiderOak Hive/"))
 
