3b93595939e299cafea1af4869b38ef4ff4590cb
Matt Erickson <peawee@peawee.net>
2015-05-04 10:22:19 -0500

    Added a convenience function for error checking boilerplate.

--- init.d/30_go.el
+++ init.d/30_go.el
@@ -9,3 +9,19 @@
 (add-to-list 'load-path (concat (getenv "GOPATH") "/src/github.com/dougm/goflymake"))
 (require 'go-flymake)
 
+(defun mce-go-funcall-err-returnp ()
+  "Determines if there's an err variable returned by the function call under point."
+  (save-excursion
+    (beginning-of-line)
+    (re-search-forward (regexp-quote "err :="))))
+
+(defun mce-go-insert-errcatch ()
+  "If there's a variable named 'err' in the return from a golang funcall, insert boilerplate error checking"
+  (interactive)
+  (when (mce-go-funcall-err-returnp)
+    (forward-line)
+    (let ((start-marker (point-marker)))
+      (insert "if err != nil {\n\n}\n")
+      (indent-region start-marker (point))
+      (forward-line -2)
+      (indent-according-to-mode))))
