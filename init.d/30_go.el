814b513cb441c6128a3bd0330b6713017880ffe3
Matt Erickson <matt@spideroak.com>
2017-02-06 12:19:32 -0600

    Updates for Go

--- init.d/30_go.el
+++ init.d/30_go.el
@@ -4,6 +4,7 @@
 (require 'go-guru)
 
 ;; Make sure we run go fmt as we save.
+(setq gofmt-command "goimports")
 (add-hook 'before-save-hook 'gofmt-before-save)
 
 (add-hook 'go-mode-hook
@@ -13,24 +14,9 @@
 (add-hook 'go-mode-hook 'go-eldoc-setup)
 (add-hook 'go-mode-hook 'nlinum-relative-mode)
 
-;; Yay flymake!
-;(add-to-list 'load-path (concat (car (split-string (getenv "GOPATH") ":")) "/src/github.com/dougm/goflymake"))
-;(require 'go-flymake)
-
-
 (defun mce-go-funcall-err-returnp ()
   "Determines if there's an err variable returned by the function call under point."
   (save-excursion
     (beginning-of-line)
     (re-search-forward (regexp-quote "err :="))))
 
-(defun mce-go-insert-errcatch ()
-  "If there's a variable named 'err' in the return from a golang funcall, insert boilerplate error checking"
-  (interactive)
-  (when (mce-go-funcall-err-returnp)
-    (forward-line)
-    (let ((start-marker (point-marker)))
-      (insert "if err != nil {\n\n}\n")
-      (indent-region start-marker (point))
-      (forward-line -2)
-      (indent-according-to-mode))))
