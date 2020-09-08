b519a71b46afb9814166be854b8a095b748fd31e
Matt Erickson <peawee@peawee.net>
2020-09-08 09:46:36 -0500

    Removed PYTHONPATH from by-host configuration

--- init.d/01_by_hostname.el
+++ init.d/01_by_hostname.el
@@ -2,12 +2,3 @@
 
 ; Defaults.
 ; TODO: make into per-hostname sometime later.
-(setenv "PYTHONPATH"
-        (concat
-         "/Users/matt/Dev/SpiderOak/pandora" ":"
-         "/Users/matt/Dev/SpiderOak/net_kes/netkes" ":"
-         "/Users/matt/Dev/SpiderOak/pandora-email" ":"
-         "/usr/local/lib/python" ":"
-         "/usr/lib/python2.7/site-packages" ":"
-         (getenv "PYTHONPATH")))
-
