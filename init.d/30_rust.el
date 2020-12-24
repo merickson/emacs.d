99a549fdc8199acfadb1cd805b054784b939c26e
Matt Erickson <matt@spideroak.com>
2020-12-24 13:02:49 -0600

    Tweaks to org-mode.

--- init.d/30_rust.el
+++ init.d/30_rust.el
@@ -15,5 +15,7 @@
 (add-hook 'rust-mode-hook #'racer-mode)
 (add-hook 'racer-mode-hook #'eldoc-mode)
 (add-hook 'racer-mode-hook #'company-mode)
-
+(require 'rust-mode)
+(define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+(setq company-tooltip-align-annotations t)
 (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
